4277c215f6a163742819650996ac2f3eb1fd1db7
Daniel <thepretki@gmail.com>
2022-10-06 19:26:44 +0200

    its done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const optCHF = document.querySelector(".option-chf");
 const selectCUR = document.querySelector(".select-currancy");
 const convertBTN = document.querySelector(".btn");
 const inptValue = document.querySelector(".input");
-const p = document.querySelector(".h5");
+const h4 = document.querySelector(".h4");
 
 function findObj(arr, key, val) {
   for (var i = 0; i < arr.length; i++) {
@@ -18,11 +18,22 @@ async function check(e) {
   return (code = e.value);
 }
 function makeMagic(a, b) {
-  return (a * b).toFixed(2);;
+  return (a * b).toFixed(2);
 }
 
-convertBTN.addEventListener("click", getExchangeRates);
-
+convertBTN.addEventListener("click", loader);
+function loader() {
+  document.querySelector(".loader-outer").classList.remove("hidden");
+  document.querySelector(".loader-outer").classList.add("d-flex");
+  setTimeout(() => {
+    getExchangeRates();
+    document.querySelector(".loader-outer").classList.add("hidden");
+    document.querySelector(".loader-outer").classList.remove("d-flex");
+  }, 2000);
+  // getExchangeRates()
+  // document.querySelector(".loader-outer").classList.add("hidden");
+  // document.querySelector(".loader-outer").classList.remove("d-flex");
+}
 async function getExchangeRates() {
   try {
     const curResponse = await fetch(
@@ -32,8 +43,8 @@ async function getExchangeRates() {
     const allRatesArr = finaleCurResponse[0].rates;
     let code = await check(selectCUR);
     let rate = findObj(allRatesArr, "code", code);
-    p.textContent =
-      `${(inptValue.valueAsNumber).toFixed(2)}` +
+    h4.textContent =
+      `${inptValue.valueAsNumber.toFixed(2)}` +
       " " +
       `${rate.code}` +
       " to " +
